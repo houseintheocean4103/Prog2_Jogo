750d146a13fe35412a094c3c6ff21a10b300a8d4
Lucas Magno <lucas_mundo_louco@hotmail.com>
2025-07-30 21:28:05 -0300

    adicionando logger no game

--- Game.java
+++ Game.java
@@ -4,21 +4,29 @@ import java.util.Scanner;
 public class Game {
 	
 	Dificuldade dif;
+	Log logger;
 	
 	public Game(Dificuldade d) {
 		
 		this.dif = d;
+		this.logger = new Log();
 		
 	}
 	
 	public void iniciar() {
+		logger.logInicioJogo(dif);
 		
 		ArrayList<Player> herois = gerar_herois();
 		ArrayList<Player> monstros = gerar_monstros();
+
+		logger.logTotalPersonagens(herois.size(), monstros.size());
 		
 		Turno turno = new Turno();
+		logger.logInicioBatalha();
 		
 		turno.batalha(herois, monstros, dif);
+		logger.logFimBatalha();
+		logger.logFimJogo("Jogo concluído");
 		
 	}
 	
@@ -28,8 +36,13 @@ public class Game {
 		ArrayList<Player> list = new ArrayList<>();
 		
 		list.add(new Ogro("monstro A"));
+		logger.logMonstroGerado("Ogro (monstro A)");
+
 		list.add(new Duende("monstro B"));
+		logger.logMonstroGerado("Duende (monstro B)");
+
 		list.add(new Alien("monstro C"));
+		logger.logMonstroGerado("Alien (monstro C)");
 		
 		return list;
 	}
@@ -49,12 +62,15 @@ public class Game {
 			h = entrada.nextInt();
 			if(h == 1) {
 				list.add(new Guerreiro("guerreiro"));
+				logger.logHeroiEscolhido("Guerreiro", i);
 			}
 			else if(h == 2) {
 				list.add(new Mago("mago"));
+				logger.logHeroiEscolhido("Mago", i);
 			}
 			else if(h == 3) {
 				list.add(new Arqueiro("arqueiro"));
+				logger.logHeroiEscolhido("Arqueiro", i);
 			}
 		}
 		
